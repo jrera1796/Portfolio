9abeb6e306d5d30ba0d2ff25187b35f3e4d10822
jrera1796 <86329054+jrera1796@users.noreply.github.com>
2023-07-25 23:44:03 -0500

    Website layout and design, new logo

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,49 +1,48 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 function Nav(props) {
-  const {
-    pages = [],
-    setCurrentPage,
-    currentPage,
-   
-  } = props;
+  const { pages = [], setCurrentPage, currentPage, menuOpen, toggleMenu } = props;
+  const menuRef = useRef(null);
+
+  // Handle click outside of the menu to close it
+  const handleOutsideClick = (event) => {
+    if (menuRef.current && !menuRef.current.contains(event.target)) {
+      toggleMenu();
+    }
+  };
 
   return (
-    <header className="flex-row px-1">
-      <div className='header-title'>
-      <h2>
-        <a data-testid="link" href="/">
-          Jose Rivera
-        </a>
-      </h2>
+    <nav className={`nav-wrapper ${menuOpen ? 'open' : ''}`}>
+      <div
+        id="menuToggle"
+        className={`menu-toggle ${menuOpen ? 'open' : ''}`}
+        onClick={toggleMenu}
+        onBlur={handleOutsideClick} // Hide the menu when it loses focus
+        onMouseLeave={handleOutsideClick} // Hide the menu when the cursor leaves the menu area
+      >
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
-      <nav><div id="menuToggle">
-      <input type="checkbox" />
-      <span></span>
-    <span></span>
-    <span></span>
-        <ul className="flex-row nav-links" id="menu">
-          
-          {pages.map( (reference) => (
+      {/* Use a ref to capture the menu container */}
+      <div ref={menuRef} className="nav-container">
+        <ul className={`nav-links ${menuOpen ? 'show' : ''}`} id="menu">
+          {pages.map((reference) => (
             <li
-              className={`mx-1 ${ currentPage.name === reference.name && 'navActive'
-              }`}
+              className={`${currentPage.name === reference.name && 'navActive'}`}
               key={reference.name}
-                
-              
-                onClick={() => {
-                  setCurrentPage(reference);
-                }}
-              >
+              onClick={() => {
+                setCurrentPage(reference);
+                toggleMenu(); // Close the menu after selecting a page
+              }}
+            >
               {reference.name}
-              
             </li>
           ))}
-       
         </ul>
         </div>
-      </nav>
-    </header>
+    </nav>
+
   );
 }
 
